a58c637a38faddb4a28bb575d6074678699b2fa4
Shamil Akhmadyhsve <deker929@gmail.com>
2023-03-30 18:10:28 +0200

    every year update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const countDown = () => {
-    const countDate = new Date('June 1, 2023 00:00:00').getTime();
+    const year = new Date().getFullYear();
+    const countDate = new Date(`June 1, ${year} 00:00:00`).getTime();
     const nowDate = new Date().getTime();
     const gap = countDate - nowDate;
 
@@ -18,5 +19,27 @@ const countDown = () => {
     document.querySelector(".countdown__minute").innerHTML = textMinute;
     document.querySelector(".countdown__second").innerHTML = textSecond;
     setTimeout(countDown, 1000);
+    if (nowDate > countDate) {
+        const year = new Date().getFullYear() + 1;
+        const countDate = new Date(`June 1, ${year} 00:00:00`).getTime();
+        const nowDate = new Date().getTime();
+
+        const gap = countDate - nowDate;
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) / hour);
+        const textMinute = Math.floor((gap % hour) / minute);
+        const textSecond = Math.floor((gap % minute) / second);
+
+        document.querySelector(".countdown__day").innerHTML = textDay;
+        document.querySelector(".countdown__hour").innerHTML = textHour;
+        document.querySelector(".countdown__minute").innerHTML = textMinute;
+        document.querySelector(".countdown__second").innerHTML = textSecond;
+        setTimeout(countDown, 1000);
+    }
 }
 setTimeout(countDown, 1000);
